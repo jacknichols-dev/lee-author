01ba5305d7072558153cec6110731b36c19f348d
Jack Nichols <jacknichols@Jacks-MacBook-Pro.local>
2021-04-10 10:51:33 +0100

    test

--- client/src/components/hero-carousel/slide-data.js
+++ client/src/components/hero-carousel/slide-data.js
@@ -1,7 +1,7 @@
 const SLIDE_DATA = [
     {
         bgImage: 'images/lee.banner.1.jpg',
-        image: 'images/mr-lucky.png',
+        image: '/images/mr-lucky.png',
         title: 'Mr. Lucky',
         author: 'Lee Wood',
         synopsis: `A single instant shattered Detective James Sheldon's perfect life. Eighteen months later, he is begging for loose change on the streets, and contemplating suicide.
@@ -10,7 +10,7 @@ const SLIDE_DATA = [
     },
     {
         bgImage: 'images/lee.banner.2.jpg',
-        image: 'images/lucky-break.png',
+        image: '/images/lucky-break.png',
         title: 'Lucky Break',
         author: 'Lee Wood',
         synopsis: `When Peter Winston-Moore discovers a genuine Picasso in a junk shop, he can't believe his luck. It sells for $5.4 million at Sotheby's. Now he keeps finding valuable items, which doesn't go unnoticed by his competitors. Three of them become curious about his secret source and decide to follow him with disastrous consequences.`,
@@ -18,7 +18,7 @@ const SLIDE_DATA = [
     },
     {
         bgImage: 'images/lee.banner.3.jpg',
-        image: 'images/dead-lucky.png',
+        image: '/images/dead-lucky.png',
         title: 'Dead Lucky',
         author: 'Lee Wood',
         synopsis: `The drug addicts are causing havoc for the police and the community and making the centre of Trentbridge a no-go zone. It's all down to a new illegal substance called ‘Monkey Dust’ that is far more powerful than Heroin, yet sells for a fraction of the price.00`,
